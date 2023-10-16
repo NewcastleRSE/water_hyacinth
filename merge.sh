1b7b276c3006b831271c533107d6c3a9822e8aff
diablorojo <rickydaly@gmail.com>
2023-10-16 11:32:19 +0000

    update to bash file

--- merge.sh
+++ merge.sh
@@ -2,6 +2,7 @@
 
 rm -rf /home/ubuntu/py/json/*.json
 python3 shptojson.py
-geojson-merge json/*.json > json/mergebig.json
+geojson-merge json/*.json > json/mergebig0.json
+mapshaper json/mergebig0.json -simplify dp 10% keep-shapes -o format=geojson json/mergebig.json
 cp json/mergebig.json /var/www/html/poly/json
 

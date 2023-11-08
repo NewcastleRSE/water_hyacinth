d1c9e5344187603a1a7020935430f0fa9fba7ee3
diablorojo <rickydaly@gmail.com>
2023-11-08 15:14:14 +0000

    Update to file

--- merge.sh
+++ merge.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
 rm -rf /home/ubuntu/py/json/*.json
-python3 shptojson.py
-geojson-merge json/*.json > json/mergebig0.json
-mapshaper json/mergebig0.json -simplify dp 10% keep-shapes -o format=geojson json/mergebig.json
-cp json/mergebig.json /var/www/html/poly/json
 
+echo "Converting shapefiles to geojson..."
+sleep 2
+python3 shptojson.py
+echo "Done!"
+
+echo "Merging json files to master json..."
+sleep 2
+geojson-merge json/*.json > json/mergebig0.json
+echo "Done!"
+
+echo "Compressing json file to smaller size..."
+sleep 2
+mapshaper json/mergebig0.json -simplify dp 10% keep-shapes -o format=geojson json/mergebig.json
+echo "Done!"
+
+echo "Copy to web directory..."
+sleep 2
+cp json/mergebig.json /var/www/html/poly/json
+echo "Done!"
